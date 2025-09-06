29795f4bcd328a23546d3c7f3adc9b9bcaf2268a
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2025-09-06 18:18:54 +0100

    fall back

--- slice.go
+++ slice.go
@@ -56,6 +56,22 @@ func (e *Elem[T]) Val() T {
 	return e.val
 }
 
+func (s *Slice[T]) availableCount() int {
+	count := 0
+	for _, item := range s.items {
+		if item.Available() {
+			count++
+		}
+	}
+	return count
+}
+
+func (s *Slice[T]) AvailableCount() int {
+	s.rw.RLock()
+	defer s.rw.RUnlock()
+	return s.availableCount()
+}
+
 func (s *Slice[T]) Append(items ...T) *Slice[T] {
 	s.rw.Lock()
 	defer s.rw.Unlock()
@@ -68,10 +84,44 @@ func (s *Slice[T]) Append(items ...T) *Slice[T] {
 func (s *Slice[T]) Get() TContext[T] {
 	s.rw.RLock()
 	defer s.rw.RUnlock()
+	if len(s.items) == 0 {
+		return nil
+	}
 	for {
-		idx := s.cur.Add(1) % int64(len(s.items))
-		if s.items[idx].Available() {
-			return s.items[idx]
+		if s.availableCount() == 0 {
+			return nil
+		}
+		if e := s.getOrNil(); e != nil {
+			return e
+		}
+	}
+}
+func (s *Slice[T]) getOrNil() *Elem[T] {
+	if len(s.items) == 0 {
+		return nil
+	}
+	idx := s.cur.Add(1) % int64(len(s.items))
+	e := s.items[idx]
+	if e.Available() {
+		return e
+	}
+	return nil
+}
+
+func (s *Slice[T]) MustGet(sleep time.Duration) TContext[T] {
+	s.rw.RLock()
+	defer s.rw.RUnlock()
+	if len(s.items) == 0 {
+		return nil
+	}
+	for {
+		if s.availableCount() == 0 {
+			time.Sleep(sleep)
+			continue
+		}
+		e := s.getOrNil()
+		if e != nil {
+			return e
 		}
 	}
 }
